2521d52403c9336451d8440a8ffde8153f4f3738
NB-Matlala <njabulo_matlala@yahoo.com>
2023-09-19 20:52:30 +0200

    Enviro Vars

--- LocalDBTest.py
+++ LocalDBTest.py
@@ -1,28 +1,31 @@
 import pyodbc
+from decouple import config
 
 # Define the database connection parameters
 server = None
 database = None
 
 # Specify the path to the abc.txt file
-file_path = 'pers.txt'
-
-# Open and read the file
-try:
-    with open(file_path, 'r') as file:
-        server = file.readline().strip()
-        database = file.readline().strip()
-except FileNotFoundError:
-    print(f"The file '{file_path}' was not found.")
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
-
+# file_path = 'pers.txt'
+#
+# # Open and read the file
+# try:
+#     with open(file_path, 'r') as file:
+#         server = file.readline().strip()
+#         database = file.readline().strip()
+# except FileNotFoundError:
+#     print(f"The file '{file_path}' was not found.")
+# except Exception as e:
+#     print(f"An error occurred: {str(e)}")
+server = config('SVR')
+database = config('DBS')
 
 # Define the SQL INSERT query
 insert_query = "INSERT INTO Employees (Name, Surname, Salary) VALUES (?, ?, ?)"
 
 # Create a list of parameter values for the INSERT query
-parameter_values = ["Mike", "Michealson", "7250,50"]
+parameter_values = ["Jeff", "Davids", "1250,50"]
+
 
 try:
     # Establish a connection to the database
