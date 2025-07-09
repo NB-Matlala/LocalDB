cab354dbb62431b63b8d9c932ba56e2b21134149
NB Matlala <njabulo_matlala@yahoo.com>
2025-07-09 13:18:19 +0200

    Update PrivatePropRes_Inside4_3.py

--- PrivatePropRes_Inside4_3.py
+++ PrivatePropRes_Inside4_3.py
@@ -100,7 +100,7 @@ def extractor(soup, url):
                 dining = value
             elif '#garages' in feat_icon:
                 garage = value
-            elif '#covered-parking' in feat_icon:
+            elif '#covered-parkiung' in feat_icon:
                 parking = value
             elif '#storeys' in feat_icon:
                 storeys = value
@@ -131,9 +131,11 @@ def extractor(soup, url):
 
 def getIds(soup):
     try:
-        script_data = soup.find('script', type='application/ld+json').string
-        json_data = json.loads(script_data)
-        url = json_data['url']
+        # script_data = soup.find('script', type='application/ld+json').string
+        # json_data = json.loads(script_data)
+        # url = json_data['url']
+        url = soup['href']
+
         prop_ID_match = re.search(r'/([^/]+)$', url)
         if prop_ID_match:
             return prop_ID_match.group(1)
@@ -150,40 +152,46 @@ gp_links = [f'{base_url}/for-sale/gauteng/bronkhorstspruit/647',
             f'{base_url}/for-sale/gauteng/gauteng-south/812']
 
 for loc in gp_links:
-    response_text = session.get(loc)
-    home_page = BeautifulSoup(response_text.content, 'html.parser')
+    # response_text = session.get(loc)
+    # home_page = BeautifulSoup(response_text.content, 'html.parser')
         
-    new_links = []
-    try:
-        inner = home_page
-        ul2 = inner.find('ul', class_='region-content-holder__unordered-list')
-        if ul2:
-            li_items2 = ul2.find_all('li', class_='region-content-holder__list')
-            for area2 in li_items2:
-                link2 = area2.find('a')
-                link2 = f"{base_url}{link2.get('href')}"
-                new_links.append(link2)
-        else:
-            new_links.append(loc)
-    except Exception as e:
-        print(f"Request failed for {loc}: {e}")
+    # new_links = []
+    # try:
+    #     inner = home_page
+    #     ul2 = inner.find('ul', class_='region-content-holder__unordered-list')
+    #     if ul2:
+    #         li_items2 = ul2.find_all('li', class_='region-content-holder__list')
+    #         for area2 in li_items2:
+    #             link2 = area2.find('a')
+    #             link2 = f"{base_url}{link2.get('href')}"
+    #             new_links.append(link2)
+    #     else:
+    #         new_links.append(loc)
+    # except Exception as e:
+    #     print(f"Request failed for {loc}: {e}")
     
-    for x in new_links:
-        try:
-            land = session.get(x)
-            land_html = BeautifulSoup(land.content, 'html.parser')
-            pgs = getPages(land_html, x)
-            for p in range(1, pgs + 1):
-                home_page = session.get(f"{x}?page={p}")
-                soup = BeautifulSoup(home_page.content, 'html.parser')
-                prop_contain = soup.find_all('a', class_='listing-result')
-                for x_page in prop_contain:
-                    prop_id = getIds(x_page)
-                    if prop_id:
-                        list_url = f"{base_url}/for-sale/something/something/something/{prop_id}"
-                        queue.put({"url": list_url, "extract_function": extractor})
-        except Exception as e:
-            print(f"Failed to process URL {x}: {e}")
+    # for x in new_links:
+    try:
+        x = loc
+
+        land = session.get(x)
+        land_html = BeautifulSoup(land.content, 'html.parser')
+        pgs = getPages(land_html, x)
+
+        for p in range(1, pgs + 1):
+            home_page = session.get(f"{x}?pt=2&page={p}")
+            # home_page = session.get(f"{x}?page={p}")
+            soup = BeautifulSoup(home_page.content, 'html.parser')
+            prop_contain = soup.find_all('a', class_='featured-listing')
+            prop_contain.extend(soup.find_all('a', class_='listing-result'))
+            for x_page in prop_contain:
+                prop_id = getIds(x_page)
+                if prop_id:
+                    list_url = f"{base_url}/for-sale/something/something/something/{prop_id}" 
+                    queue.put({"url": list_url, "extract_function": extractor})
+
+    except Exception as e:
+        print(f"Failed to process URL {x}: {e}")
 
 # Start threads
 num_threads = 10  # Adjust the number of threads based on your system's capabilities
@@ -223,4 +231,3 @@ with open(csv_filename, "rb") as data:
 
 print("CSV file uploaded to Azure Blob Storage.")
 # driver.quit()
-
