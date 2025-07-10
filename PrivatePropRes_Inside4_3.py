022282d19c5906b97dd5492b933c95d7e97072b8
NB Matlala <njabulo_matlala@yahoo.com>
2025-07-10 15:39:29 +0200

    Update PrivatePropRes_Inside4_3.py

--- PrivatePropRes_Inside4_3.py
+++ PrivatePropRes_Inside4_3.py
@@ -181,7 +181,7 @@ for loc in gp_links:
         pgs = getPages(land_html, x)
 
         for p in range(1, pgs + 1):
-            home_page = session.get(f"{x}?pt=2&page={p}")
+            home_page = session.get(f"{x}?page={p}")
             # home_page = session.get(f"{x}?page={p}")
             soup = BeautifulSoup(home_page.content, 'html.parser')
             prop_contain = soup.find_all('a', class_='featured-listing')
