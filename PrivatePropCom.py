3be33ae792907da7e751f9ddee5772372098be15
NB Matlala <njabulo_matlala@yahoo.com>
2025-12-18 11:58:12 +0200

    Refactor getPages to use updated div class

--- PrivatePropCom.py
+++ PrivatePropCom.py
@@ -30,7 +30,8 @@ async def fetch(session, url):
 
 def getPages(soupPage, url):
     try:
-        num_pg = soupPage.find('div', class_='listing-results-layout__mobile-item-count txt-small-regular')
+        num_pg = soupPage.find('div', class_='sort-and-listing-count')
+        ('div', class_='listing-results-layout__mobile-item-count txt-small-regular')
         num_pgV = num_pg.text.split('of ')[-1]
         num_pgV = num_pgV.replace('\xa0', '').replace(' results', '')
         pages = math.ceil(int(num_pgV) / 20)
